be6eb933507c763542af2b3735bce34cad057637
Jalen <webadmin@myactionspot.com>
2024-03-07 16:27:56 -0800

    converted datetime objects to time objects. if user wants to book meeting and there is a time conflict, it will not go through

--- meetingroom/core/utils/google_calendar_utils.py
+++ meetingroom/core/utils/google_calendar_utils.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, time
 from typing import List, Tuple
 from django.utils import timezone
 
@@ -18,48 +18,61 @@ def sort_appointments(appointments: List[dict]) -> List[Tuple[datetime, datetime
     return sorted(sorted_appointments)
 
 
+def convert_datetime_to_time(
+    appointment: Tuple[datetime, datetime]
+) -> Tuple[time, time]:
+    return appointment[0].time(), appointment[1].time()
+
+
 def is_current_time_between(start_time: datetime, end_time: datetime) -> bool:
     local_timezone = timezone.get_current_timezone()
     current_time = datetime.now(local_timezone)
     return start_time <= current_time <= end_time
 
 
-def get_appointments(calendar_service) -> List[Tuple[datetime, datetime]]:
+def get_appointments(calendar_service) -> List[Tuple[time, time]]:
     appointments_data: list = calendar_service.get_events()
     appointments: List[Tuple[datetime, datetime]] = sort_appointments(appointments_data)
+
+    if not appointments:
+        return []
+
+    # Convert appointments to datetime.time
+    appointments = [(convert_datetime_to_time(start_end)) for start_end in appointments]
     print("booked appointments", appointments)
     return appointments
 
 
-def get_available_time_slots(appointments: List[Tuple[datetime, datetime]]):
-    available_time_slots = []
-    business_hours = [
-        "8:00 AM",
-        "7:00 PM",
-    ]
+def get_available_time_slots(appointments: List[Tuple[time, time]]) -> List:
 
-    if len(appointments) == 1:
-        available_time_slots = [
-            ["now", appointments[0][0].strftime("%#I:%M %p")],
-            [appointments[-1][1].strftime("%#I:%M %p"), business_hours[1]],
-        ]
-        return available_time_slots
+    business_hours = (
+        datetime.strptime("8:00 AM", "%I:%M %p").time(),
+        datetime.strptime("7:00 PM", "%I:%M %p").time(),
+    )
+    local_timezone = timezone.get_current_timezone()
+    business_hours = tuple(dt.replace(tzinfo=local_timezone) for dt in business_hours)
+    print("business hours: ", business_hours)
+    if not appointments:
+        return [business_hours]
+
+    available_time_slots = []
+    current_time = datetime.now().astimezone().time()
+    print("current time", current_time)
+
+    if business_hours[0] < current_time <= appointments[0][0]:
+        available_time_slots.append([current_time, appointments[0][0]])
 
     for i in range(1, len(appointments)):
         previous_end = appointments[i - 1][1]
         current_start = appointments[i][0]
 
-        previous_end_12hr = previous_end.strftime("%#I:%M %p")
-        current_start_12hr = current_start.strftime("%#I:%M %p")
-
         if previous_end >= current_start:
             continue
-        available_time_slots.append([previous_end_12hr, current_start_12hr])
+        available_time_slots.append([previous_end, current_start])
 
-    if datetime.strptime(available_time_slots[-1][-1], "%#I:%M %p") < datetime.strptime(
-        business_hours[1], "%#I:%M %p"
-    ):
-        available_time_slots.append([[available_time_slots[-1][-1]], business_hours[1]])
     print("available time slots", available_time_slots)
+
+    if available_time_slots[-1][-1] <= business_hours[1]:
+        available_time_slots.append([available_time_slots[-1][-1], business_hours[1]])
 
     return available_time_slots
--- meetingroom/core/views.py
+++ meetingroom/core/views.py
@@ -1,10 +1,13 @@
 from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
 from googleapiclient.errors import HttpError
-from .utils import *
-from .forms import EventForm
+from django.utils import timezone
 from datetime import datetime, date, time
 from typing import Optional
+
+from .utils import *
+from .forms import EventForm
+
 
 calendar_service: GoogleCalendarService = GoogleCalendarService()
 
@@ -30,6 +33,13 @@ def book_reservation(req: HttpRequest) -> HttpResponse:
     try:
         appointments = get_appointments(calendar_service)
         available_time_slots = get_available_time_slots(appointments)
+        available_time_slots_formatted = []
+
+        for time_slot in available_time_slots:
+            start = time_slot[0].strftime("%#I:%M %p")
+            end = time_slot[1].strftime("%#I:%M %p")
+
+            available_time_slots_formatted.append([start, end])
 
     except Exception as e:
         print(f"An error occurred: {str(e)}")
@@ -37,11 +47,11 @@ def book_reservation(req: HttpRequest) -> HttpResponse:
 
     if req.method != "POST":
         form: EventForm = EventForm()
-        context = {"form": form, "available_time_slots": available_time_slots}
+        context = {"form": form, "available_time_slots": available_time_slots_formatted}
         return render(req, "create_event.html", context)
 
     form: EventForm = EventForm(req.POST)
-    context = {"form": form, "available_time_slots": available_time_slots}
+    context = {"form": form, "available_time_slots": available_time_slots_formatted}
 
     if not form.is_valid():
         return render(req, "create_event.html", context)
@@ -50,6 +60,9 @@ def book_reservation(req: HttpRequest) -> HttpResponse:
     start_time_str: Optional[str] = form.cleaned_data.get("start_time")
     end_time_str: Optional[str] = form.cleaned_data.get("end_time")
     email: str = form.cleaned_data.get("email", "")
+
+    if not all([name, start_time_str, end_time_str, email]):
+        return render(req, "error.html", {"error_message": "Missing required data"})
 
     try:
         start_time: time = (
@@ -61,15 +74,37 @@ def book_reservation(req: HttpRequest) -> HttpResponse:
             datetime.strptime(end_time_str, "%H:%M").time() if end_time_str else time()
         )
 
-        today: date = date.today()
-        start_datetime: datetime = datetime.combine(today, start_time)
-        end_datetime: datetime = datetime.combine(today, end_time)
+        # today: date = date.today()
+        # start_datetime: datetime = timezone.make_aware(
+        #     datetime.combine(today, start_time), timezone.get_current_timezone()
+        # )
+        # print("start_datetime: ", start_datetime)
+        # end_datetime: datetime = timezone.make_aware(
+        #     datetime.combine(today, end_time), timezone.get_current_timezone()
+        # )
+        # print("end_datetime: ", end_datetime)
 
-        start_formatted: str = start_datetime.strftime("%Y-%m-%dT%H:%M:%S%z")
-        end_formatted: str = end_datetime.strftime("%Y-%m-%dT%H:%M:%S%z")
+        # start_formatted: str = start_datetime.strftime("%Y-%m-%dT%H:%M:%S%z")
+        # end_formatted: str = end_datetime.strftime("%Y-%m-%dT%H:%M:%S%z")
 
-        if not all([name, start_time_str, end_time_str, email]):
-            return render(req, "error.html", {"error_message": "Missing required data"})
+        start_formatted: str = start_time.strftime("%#I %M %p")
+        end_formatted: str = end_time.strftime("%#I %M %p")
+
+        # TODO: check if there is an overlap
+        """
+        1. need to compare with datetime objects => make sure we use datetimeobjects only, not strings
+                                                 => create function for printing strings of datetime objects
+
+        """
+
+        for time_slots in available_time_slots:
+            if (
+                start_time < time_slots[0]
+                and end_time >= time_slots[0]
+                or time_slots[0] <= start_time < time_slots[1]
+            ):
+                context["has_time_conflict"] = True
+                return render(req, "create_event.html", context)
 
         calendar_service.create_event(name, email, start_formatted, end_formatted)
         return render(req, "success.html", {"message": "Event scheduled successfully"})

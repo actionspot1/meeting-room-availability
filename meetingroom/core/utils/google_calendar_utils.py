1e633079ec20460679d2f6d4b730a7687dfe4893
Jalen <webadmin@myactionspot.com>
2024-03-08 14:29:19 -0800

    fixed bug sending payload to google api. fixed bug when there are no booked appointments.

--- meetingroom/core/utils/google_calendar_utils.py
+++ meetingroom/core/utils/google_calendar_utils.py
@@ -30,6 +30,18 @@ def is_current_time_between(start_time: datetime, end_time: datetime) -> bool:
     return start_time <= current_time <= end_time
 
 
+def get_current_time() -> time:
+    local_timezone = timezone.get_current_timezone()
+    return datetime.now(local_timezone).astimezone().time()
+
+
+def get_business_hours() -> Tuple[time, time]:
+    return (
+        datetime.strptime("8:00 AM", "%I:%M %p").time(),
+        datetime.strptime("7:00 PM", "%I:%M %p").time(),
+    )
+
+
 def get_appointments(calendar_service) -> List[Tuple[time, time]]:
     appointments_data: list = calendar_service.get_events()
     appointments: List[Tuple[datetime, datetime]] = sort_appointments(appointments_data)
@@ -37,27 +49,24 @@ def get_appointments(calendar_service) -> List[Tuple[time, time]]:
     if not appointments:
         return []
 
-    # Convert appointments to datetime.time
-    appointments = [(convert_datetime_to_time(start_end)) for start_end in appointments]
-    print("booked appointments", appointments)
+    appointments = [convert_datetime_to_time(start_end) for start_end in appointments]
+    print("appointments: ", appointments)
     return appointments
 
 
 def get_available_time_slots(appointments: List[Tuple[time, time]]) -> List:
+    if not appointments:
+        return []
 
-    business_hours = (
-        datetime.strptime("8:00 AM", "%I:%M %p").time(),
-        datetime.strptime("7:00 PM", "%I:%M %p").time(),
-    )
+    business_hours = get_business_hours()
     local_timezone = timezone.get_current_timezone()
     business_hours = tuple(dt.replace(tzinfo=local_timezone) for dt in business_hours)
     print("business hours: ", business_hours)
-    if not appointments:
-        return [business_hours]
+
+    current_time = get_current_time()
+    print("current time", current_time)
 
     available_time_slots = []
-    current_time = datetime.now().astimezone().time()
-    print("current time", current_time)
 
     if business_hours[0] < current_time <= appointments[0][0]:
         available_time_slots.append([current_time, appointments[0][0]])
@@ -70,9 +79,8 @@ def get_available_time_slots(appointments: List[Tuple[time, time]]) -> List:
             continue
         available_time_slots.append([previous_end, current_start])
 
-    print("available time slots", available_time_slots)
-
-    if available_time_slots[-1][-1] <= business_hours[1]:
+    if available_time_slots and available_time_slots[-1][-1] <= business_hours[1]:
         available_time_slots.append([available_time_slots[-1][-1], business_hours[1]])
+    print("available time slots", available_time_slots)
 
     return available_time_slots
--- meetingroom/core/views.py
+++ meetingroom/core/views.py
@@ -32,9 +32,12 @@ def index(req: HttpRequest) -> HttpResponse:
 def book_reservation(req: HttpRequest) -> HttpResponse:
     try:
         appointments = get_appointments(calendar_service)
-        available_time_slots = get_available_time_slots(appointments)
-        available_time_slots_formatted = []
+        business_hours: Tuple[time, time] = get_business_hours()
 
+        available_time_slots = get_available_time_slots(appointments)
+
+        available_time_slots_formatted = []
+        print("available_time_slots")
         for time_slot in available_time_slots:
             start = time_slot[0].strftime("%#I:%M %p")
             end = time_slot[1].strftime("%#I:%M %p")
@@ -47,11 +50,19 @@ def book_reservation(req: HttpRequest) -> HttpResponse:
 
     if req.method != "POST":
         form: EventForm = EventForm()
-        context = {"form": form, "available_time_slots": available_time_slots_formatted}
+        context = {
+            "form": form,
+            "available_time_slots": available_time_slots_formatted,
+            "business_hours": business_hours,
+        }
         return render(req, "create_event.html", context)
 
     form: EventForm = EventForm(req.POST)
-    context = {"form": form, "available_time_slots": available_time_slots_formatted}
+    context = {
+        "form": form,
+        "available_time_slots": available_time_slots_formatted,
+        "business_hours": business_hours,
+    }
 
     if not form.is_valid():
         return render(req, "create_event.html", context)
@@ -74,21 +85,21 @@ def book_reservation(req: HttpRequest) -> HttpResponse:
             datetime.strptime(end_time_str, "%H:%M").time() if end_time_str else time()
         )
 
-        # today: date = date.today()
-        # start_datetime: datetime = timezone.make_aware(
-        #     datetime.combine(today, start_time), timezone.get_current_timezone()
-        # )
+        today: date = date.today()
+        start_datetime: datetime = timezone.make_aware(
+            datetime.combine(today, start_time), timezone.get_current_timezone()
+        )
         # print("start_datetime: ", start_datetime)
-        # end_datetime: datetime = timezone.make_aware(
-        #     datetime.combine(today, end_time), timezone.get_current_timezone()
-        # )
+        end_datetime: datetime = timezone.make_aware(
+            datetime.combine(today, end_time), timezone.get_current_timezone()
+        )
         # print("end_datetime: ", end_datetime)
 
-        # start_formatted: str = start_datetime.strftime("%Y-%m-%dT%H:%M:%S%z")
-        # end_formatted: str = end_datetime.strftime("%Y-%m-%dT%H:%M:%S%z")
+        start_datetime_formatted: str = start_datetime.strftime("%Y-%m-%dT%H:%M:%S%z")
+        end_datetime_formatted: str = end_datetime.strftime("%Y-%m-%dT%H:%M:%S%z")
 
-        start_formatted: str = start_time.strftime("%#I %M %p")
-        end_formatted: str = end_time.strftime("%#I %M %p")
+        start_time_formatted: str = start_time.strftime("%#I %M %p")
+        end_time_formatted: str = end_time.strftime("%#I %M %p")
 
         # TODO: check if there is an overlap
         """
@@ -96,17 +107,19 @@ def book_reservation(req: HttpRequest) -> HttpResponse:
                                                  => create function for printing strings of datetime objects
 
         """
+        if appointments:
+            for time_slots in available_time_slots:
+                if (
+                    start_time < time_slots[0]
+                    and end_time >= time_slots[0]
+                    or time_slots[0] <= start_time < time_slots[1]
+                ):
+                    context["has_time_conflict"] = True
+                    return render(req, "create_event.html", context)
 
-        for time_slots in available_time_slots:
-            if (
-                start_time < time_slots[0]
-                and end_time >= time_slots[0]
-                or time_slots[0] <= start_time < time_slots[1]
-            ):
-                context["has_time_conflict"] = True
-                return render(req, "create_event.html", context)
-
-        calendar_service.create_event(name, email, start_formatted, end_formatted)
+        calendar_service.create_event(
+            name, email, start_datetime_formatted, end_datetime_formatted
+        )
         return render(req, "success.html", {"message": "Event scheduled successfully"})
 
     except Exception as e:

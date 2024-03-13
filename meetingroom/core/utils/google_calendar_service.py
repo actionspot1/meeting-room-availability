7d8afb06c8a99aeff120e0a671558bd2977f8d52
Jalen <webadmin@myactionspot.com>
2024-03-13 13:04:22 -0700

    modified calendar service to follow singleton pattern

--- meetingroom/core/utils/google_calendar_service.py
+++ meetingroom/core/utils/google_calendar_service.py
@@ -10,27 +10,31 @@ import os.path
 
 
 class GoogleCalendarService:
+    _instance = None
 
     SCOPES = ["https://www.googleapis.com/auth/calendar"]
 
-    def __init__(self) -> None:
-        self.service: build = self.authenticate()
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+            cls._instance.service = cls._instance.authenticate()
+        return cls._instance
 
-    def authenticate(self) -> build:
-        creds: Credentials = self.load_credentials(self.SCOPES)
+    def authenticate(self):
+        creds = self.load_credentials(self.SCOPES)
 
         if not creds or not creds.valid:
             creds = self.refresh_credentials(creds)
 
         return build("calendar", "v3", credentials=creds) if creds else None
 
-    def load_credentials(self, scopes: list) -> Credentials:
-        creds: Credentials = None
+    def load_credentials(self, scopes: list):
+        creds = None
         if os.path.exists("token.json"):
             creds = Credentials.from_authorized_user_file("token.json", scopes)
         return creds
 
-    def refresh_credentials(self, creds: Credentials) -> Credentials:
+    def refresh_credentials(self, creds):
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
@@ -38,25 +42,23 @@ class GoogleCalendarService:
             self.save_credentials(creds)
         return creds
 
-    def run_local_server(self) -> Credentials:
-        flow: InstalledAppFlow = InstalledAppFlow.from_client_secrets_file(
+    def run_local_server(self):
+        flow = InstalledAppFlow.from_client_secrets_file(
             "credentials.json", self.SCOPES
         )
         return flow.run_local_server(port=0)
 
-    def save_credentials(self, creds: Credentials) -> None:
+    def save_credentials(self, creds):
         with open("token.json", "w") as token:
             token.write(creds.to_json())
 
-    def create_event(
-        self, name: str, email: str, start_time: str, end_time: str
-    ) -> None:
+    def create_event(self, name: str, email: str, start_time: str, end_time: str):
         if not self.service or not all([name, email, start_time, end_time]):
             print("Google Calendar API authentication failed or missing parameters.")
             return
 
         try:
-            event: dict = {
+            event = {
                 "start": {"dateTime": start_time, "timeZone": "America/Los_Angeles"},
                 "end": {"dateTime": end_time, "timeZone": "America/Los_Angeles"},
                 "attendees": [{"displayName": name, "email": email}],
@@ -69,23 +71,23 @@ class GoogleCalendarService:
         except Exception as e:
             print(f"An error occurred: {str(e)}")
 
-    def get_events(self) -> list:
+    def get_events(self):
         if not self.service:
             print("Google Calendar API authentication failed.")
             return []
 
         try:
-            local_timezone: timezone = timezone.get_current_timezone()
-            now: datetime = datetime.now(local_timezone)
-            start_time: str = now.isoformat()
-            end_of_day: str = (
+            local_timezone = timezone.get_current_timezone()
+            now = datetime.now(local_timezone)
+            start_time = now.isoformat()
+            end_of_day = (
                 datetime.combine(now.date(), time(hour=23, minute=59, second=59))
                 .astimezone(local_timezone)
                 .isoformat()
             )
-            events_result: dict = self.list_events(start_time, end_of_day)
+            events_result = self.list_events(start_time, end_of_day)
 
-            events_items: list = events_result.get("items", [])
+            events_items = events_result.get("items", [])
             return events_items
 
         except exceptions.GoogleAuthError as auth_error:
@@ -96,8 +98,8 @@ class GoogleCalendarService:
             print(f"An error occurred: {str(e)}")
             return []
 
-    def list_events(self, start_time: str, end_time: str) -> dict:
-        events: build = self.service.events()
+    def list_events(self, start_time, end_time):
+        events = self.service.events()
         return events.list(
             calendarId="primary", timeMin=start_time, timeMax=end_time
         ).execute()

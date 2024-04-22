b008d965ace4b9c0c5ce36a437df87e43f2fa06d
Jalen <webadmin@myactionspot.com>
2024-04-22 08:22:54 -0700

    added default values for form. removed some comments

--- meetingroom/core/forms.py
+++ meetingroom/core/forms.py
@@ -6,19 +6,23 @@ class EventForm(forms.Form):
     name = forms.CharField(max_length=100)
     email = forms.EmailField()
 
+    number_of_people = forms.IntegerField(
+        label="Number of People Attending",
+        min_value=1,
+        initial=1,
+    )
+
     start_datetime = forms.DateTimeField(
         label="Start Date and Time",
         widget=forms.DateTimeInput(attrs={"type": "datetime-local"}),
+        initial=datetime.now(),
     )
     end_datetime = forms.DateTimeField(
         label="End Date and Time",
         widget=forms.DateTimeInput(attrs={"type": "datetime-local"}),
+        initial=datetime.now() + timedelta(hours=1),
     )
 
-    # def __init__(self, *args, **kwargs):
-    #     super(EventForm, self).__init__(*args, **kwargs)
-
-    # custom validation
     def clean(self):
         cleaned_data = super().clean()
         start_datetime = cleaned_data.get("start_datetime")

9d6abe63d46e877e7a87170d60bf4a96edd5c307
Jalen <webadmin@myactionspot.com>
2024-04-24 17:11:56 -0700

    this feature allows scheduling for 3 rooms. Does not take into account n rooms.

--- meetingroom/core/utils/google_calendar_utils.py
+++ meetingroom/core/utils/google_calendar_utils.py
@@ -10,14 +10,38 @@ def parse_iso_datetime(datetime_str: str) -> datetime:
     return datetime.fromisoformat(datetime_str)
 
 
-def sort_appointments(appointments: List[dict]) -> List[Tuple[datetime, datetime]]:
-    sorted_appointments: list[tuple[datetime, datetime]] = [
-        (
-            parse_iso_datetime(appointment["start"]["dateTime"]),
-            parse_iso_datetime(appointment["end"]["dateTime"]),
+def sort_appointments(
+    appointments: List[dict],
+) -> List[Tuple[datetime, datetime, int, str]]:
+    # sorted_appointments: list[tuple[datetime, datetime, int, str]] = [
+    #     (
+    #         parse_iso_datetime(appointment["start"]["dateTime"]),
+    #         parse_iso_datetime(appointment["end"]["dateTime"]),
+    #         appointment["additionalGuests"],
+    #         appointment["summary"],
+    #     )
+    #     for appointment in appointments
+    # ]
+
+    sorted_appointments: list[tuple[datetime, datetime, int, str]] = []
+    for appointment in appointments:
+        # print("appointment", appointment)
+        # print("attendees", appointment["attendees"][0]["additionalGuests"])
+        if not appointment["start"].get("dateTime") or not appointment["end"].get(
+            "dateTime"
+        ):
+            print("Skipping due to missing start or end time")
+            continue
+        sorted_appointments.append(
+            (
+                parse_iso_datetime(appointment["start"]["dateTime"]),
+                parse_iso_datetime(appointment["end"]["dateTime"]),
+                appointment["attendees"][0].get("additionalGuests", -1),
+                appointment["summary"],
+            )
         )
-        for appointment in appointments
-    ]
+    print("sorted appointments", sorted_appointments)
+
     return sorted(sorted_appointments)
 
 
@@ -32,9 +56,11 @@ def get_current_datetime() -> datetime:
     return datetime.now(local_timezone).astimezone()
 
 
-def get_appointments() -> List[Tuple[datetime, datetime]]:
+def get_appointments() -> List[Tuple[datetime, datetime, int, str]]:
     appointments_data: list = calendar_service.get_events()
-    appointments: List[Tuple[datetime, datetime]] = sort_appointments(appointments_data)
+    appointments: List[Tuple[datetime, datetime, int, str]] = sort_appointments(
+        appointments_data
+    )
 
     if not appointments:
         return []
@@ -50,6 +76,7 @@ def format_time_slots(time_slots: List[Tuple[datetime, datetime]]) -> List[List[
     ]
 
 
+# events: sort events
 def appointments_overlap(
     start_datetime: datetime, end_datetime: datetime, number_of_people: int
 ) -> Tuple[bool, str]:
@@ -60,22 +87,29 @@ def appointments_overlap(
     if end_datetime < get_current_datetime():
         return (True, "Error")
 
-    appointments: List[Tuple[datetime, datetime]] = get_appointments()
+    # appointments = calendar_service.get_events()
+    appointments: List[Tuple[datetime, datetime, int, str]] = get_appointments()
 
     if not appointments:
         return (False, "Launchpad")
+
+    # for appointment in appointments:
+    #     print(appointment)
+    #     parse_iso_datetime(appointment["start"]["dateTime"])
+    #     parse_iso_datetime(appointment["end"]["dateTime"])
+    # appointments.sort(key=appointments["start"]["dateTime"])
+    # print("appointments", appointments)
 
     is_launchpad_booked: bool = False
     is_wall_street_booked: bool = False
 
     for time_slot in appointments:
-        attendees = time_slot["attendees"]
-        if 1 <= number_of_people <= 4 and 4 - 1 < attendees["additionalGuests"] <= 10:
+        print("time slot", time_slot)
+        print("additional guests", time_slot[2])
+
+        if 1 <= number_of_people <= 4 and 4 - 1 < time_slot[2] <= 10:
             continue
-        if (
-            4 < number_of_people <= 10
-            and 1 - 1 <= attendees["additionalGuests"] <= 4 - 1
-        ):
+        if 4 < number_of_people <= 10 and 1 - 1 <= time_slot[2] <= 4 - 1:
             continue
 
         # if 4 < number_of_people <= 10 and time_slot["summary"] == "Radio City":
@@ -105,21 +139,24 @@ def appointments_overlap(
             or (time_slot[0] < end_datetime <= time_slot[1])
         ):
 
-            if 4 < number_of_people <= 10 and time_slot["summary"] == "Radio City":
+            if 4 < number_of_people <= 10 and time_slot[3] == "Radio City":
                 return (True, "Radio City")
 
             if 1 <= number_of_people <= 4:
-                if time_slot["summary"] == "Launchpad":
+                if time_slot[3] == "Launchpad":
                     is_launchpad_booked = True
-                elif time_slot["summary"] == "Wall Street":
+                elif time_slot[3] == "Wall Street":
                     is_wall_street_booked = True
 
         if is_launchpad_booked and is_wall_street_booked:
             return (True, "Both")
 
-    if not is_launchpad_booked:
-        return (False, "Launchpad")
-    if not is_wall_street_booked:
+    if 1 <= number_of_people <= 4:
+        if not is_launchpad_booked:
+            return (False, "Launchpad")
+        if not is_wall_street_booked:
+            return (False, "Wall Street")
+    elif 4 < number_of_people <= 10:
         return (False, "Wall Street")
 
     return (True, "end of func")
